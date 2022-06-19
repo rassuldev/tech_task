fd79782a0cba0dd79f498d99a3d72c2c172de49c
rassuldev <rassul.sarsembayevs@gmail.com>
2022-06-19 20:27:46 +0600

    manage.py - get values from google spreadsheet

--- manage.py
+++ manage.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from pprint import pprint
+import httplib2
+import googleapiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+    CREDENTIALS_FILE = 'credentials.json'  # имя файла с закрытым ключом
+    spreadsheet_id = '1iiZtElyHPvPVsEzPeqMjcxvZpXFDK5S11DeSM2xFQgw'  # file google docs
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
+                                                                   ['https://www.googleapis.com/auth/spreadsheets',
+                                                                    'https://www.googleapis.com/auth/drive'])
+    httpAuth = credentials.authorize(httplib2.Http())
+    service = googleapiclient.discovery.build('sheets', 'v4', http=httpAuth)
+
+    values = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id,
+        range='A1:E10',
+        majorDimension='ROWS'
+
+    ).execute()
+    pprint(values)
+    exit()
+
 
 
 if __name__ == '__main__':
